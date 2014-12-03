f8b09b99b08d817d1fb473a902f2443218978ed5
Yuxin David Huang <dhuang@colgate.edu>
2014-12-03 13:44:10 -0500

    Added string as a value

--- core_interp.hs
+++ core_interp.hs
@@ -3,30 +3,38 @@ module CoreInterp where
 type Name = String
 
 data Term = Var Name
-		  | Con Int 
+		  | ConI Int
+		  | ConS String
 		  | Add Term Term
 		  | Lam Term Term
 		  | App Term Term
 
-data Value = Wrong
-		   | Num Int
-		   | Fun (Value -> (Monad Value))
+data Monad m => Value = Wrong
+		   			  | Num Int
+		   			  | Str String
+		   			  | Fun (Value -> Value)
 
 type Environment = [(Name, Value)]
 
 showval :: Value -> String
 showval Wrong = "error"
 showval (Num i) = show i
+showval (Str s) = s
 showval (Fun f) = "<function>"
 
 interp :: (Monad m) => Term -> Environment -> m Value
 interp (Var x) e = look_up x e
-interp (Con i) e = return (Num i)
+interp (ConI i) e = return (Num i)
+interp (ConS s) e = return (Str s)
 interp (Add u v) e = do
 					 a <- interp u e
 					 b <- interp v e
 					 add a b
-interp (Lam x v) e = return (Fun (\a -> interp v ((x, a):e)))
+interp (Cat u v) e = do
+					 s1 <- interp u e
+					 s2 <- interp v e
+					 cat u v
+interp (Lam x v) e = return (Fun (\a -> interp v ((x,a):e)))
 interp (App t u) e = do
 					 f <- interp t e
 					 a <- interp u e
@@ -35,8 +43,8 @@ interp (App t u) e = do
 look_up :: (Monad m) => Name -> Environment -> m Value
 look_up x [] = return Wrong
 look_up x ((y,b):e) = if x == y
-	then return b
-	else look_up x e
+						 then return b
+						 else look_up x e
 
 add :: (Monad m) => Value -> Value -> m Value
 add (Num i) (Num j) = return (Num (i + j))
@@ -46,5 +54,7 @@ apply :: (Monad m) => Value -> Value -> m Value
 apply (Fun k) a = k a
 apply _ a = return Wrong
 
-
+cat :: (Monad m) => Value -> Value -> m Value
+cat (Str s1) (Str s2) = return (s1 ++ s2)
+cat _ _ = return Wrong
 
