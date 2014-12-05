09fa16bb3848636195a050ca4911c013632c95ee
C.J. Jennings <cfjennings3@gmail.com>
2014-12-04 22:19:14 -0500

    Substituted the Exception monad, and it loads!

--- core_interp.hs
+++ core_interp.hs
@@ -12,9 +12,18 @@ data Term = Var Name
 data Value = Wrong
 		   | Num Int
 		   | Str String
-		   | Fun (Value -> Value)--should eventually be like V -> m V
+		   | Fun (Value -> E Value)--should eventually be like V -> m V
 
 type Environment = [(Name, Value)]
+
+data E a = Success a | Error String deriving Show
+
+instance Monad E where
+	return a = Success a
+	Success a >>= f = f a
+	Error s >>= f = Error s
+
+errorE s = Error s
 
 showval :: Value -> String
 showval Wrong = "error"
@@ -22,7 +31,7 @@ showval (Num i) = show i
 showval (Str s) = s
 showval (Fun f) = "<function>"
 
-interp :: (Monad m) => Term -> Environment -> m Value
+interp :: Term -> Environment -> E Value
 interp (Var x) e = look_up x e
 interp (ConI i) e = return (Num i)
 interp (ConS s) e = return (Str s)
@@ -35,23 +44,23 @@ interp (App t u) e = do
 					 f <- interp t e
 					 a <- interp u e
 					 apply f a
---I don't think we interp Cat, I think we apply cat
+--I don't think we interp Cat, I think we do apply((Fun cat) a)
 
-look_up :: (Monad m) => Name -> Environment -> m Value
-look_up x [] = return Wrong
+look_up :: Name -> Environment -> E Value
+look_up x [] = errorE ("variable not in scope: " ++ x)
 look_up x ((y,b):e) = if x == y
 					  then return b
 					  else look_up x e
 
-add :: (Monad m) => Value -> Value -> m Value
+add :: Value -> Value -> E Value
 add (Num i) (Num j) = return (Num (i + j))
-add _ _ = return Wrong
+add a b = errorE ("should be numbers: " ++ showval a ++ ", " ++ showval b)
 
-apply :: (Monad m) => Value -> Value -> m Value
+apply :: Value -> Value -> E Value
 apply (Fun k) a = k a
-apply _ a = return Wrong
+apply f a = errorE ("should be function: " ++ showval f)
 
-cat :: (Monad m) => Value -> Value -> m Value
+cat :: Value -> Value -> E Value
 cat (Str s1) (Str s2) = return (Str (s1 ++ s2))
-cat _ _ = return Wrong
+cat a b = errorE ("should be strings: " ++ showval a ++ ", " ++ showval b)
 
