d4a86e1c05fd859ab91938592fb1bc64bc433581
C.J. Jennings <cfjennings3@gmail.com>
2014-12-12 00:13:41 -0500

    Reinclude interp Cat, Add Chars and Lists, and derived Show for output

--- core_interp.hs
+++ core_interp.hs
@@ -5,14 +5,24 @@ type Name = String
 data Term = Var Name
 		  | ConI Int
 		  | ConS String
+		  | ConC Char
+		  | ConL [Value]
 		  | Add Term Term
+		  | Cat Term Term
 		  | Lam Name Term
 		  | App Term Term
 
-data Value = Wrong
-		   | Num Int
+data Value = Num Int
 		   | Str String
+		   | Ch Char
+		   | List [Value]
 		   | Fun (Value -> E Value)--should eventually be like V -> m V
+instance Show Value where
+	show (Num n) = show n
+	show (Str s) = show s
+	show (Ch c) = show c
+	show (List l) = show l
+	show (Fun f) = "<function>"
 
 type Environment = [(Name, Value)]
 
@@ -29,16 +39,18 @@ instance Monad E where
 
 errorE s = Error s
 
-showval :: Value -> String
-showval Wrong = "error"
-showval (Num i) = show i
-showval (Str s) = s
-showval (Fun f) = "<function>"
+--showval :: Value -> String
+--showval Wrong = "error"
+--showval (Num i) = show i
+--showval (Str s) = s
+--showval (Fun f) = "<function>"
 
 interp :: Term -> Environment -> E Value
 interp (Var x) e = look_up x e
 interp (ConI i) e = return (Num i)
 interp (ConS s) e = return (Str s)
+interp (ConC c) e = return (Ch c)
+interp (ConL l) e = return (List l)
 interp (Add u v) e = do
 					 a <- interp u e
 					 b <- interp v e
@@ -48,7 +60,10 @@ interp (App t u) e = do
 					 f <- interp t e
 					 a <- interp u e
 					 apply f a
---I don't think we interp Cat, I think we do apply((Fun cat) a)
+interp (Cat u v) e = do
+					 s1 <- interp u e
+					 s2 <- interp v e
+					 cat s1 s2
 
 look_up :: Name -> Environment -> E Value
 look_up x [] = errorE ("variable not in scope: " ++ x)
@@ -58,13 +73,14 @@ look_up x ((y,b):e) = if x == y
 
 add :: Value -> Value -> E Value
 add (Num i) (Num j) = return (Num (i + j))
-add a b = errorE ("should be numbers: " ++ showval a ++ ", " ++ showval b)
+add a b = errorE ("should be numbers: " ++ show a ++ ", " ++ show b)
 
 apply :: Value -> Value -> E Value
 apply (Fun k) a = k a
-apply f a = errorE ("should be function: " ++ showval f)
+apply f a = errorE ("should be function: " ++ show f)
 
 cat :: Value -> Value -> E Value
 cat (Str s1) (Str s2) = return (Str (s1 ++ s2))
-cat a b = errorE ("should be strings: " ++ showval a ++ ", " ++ showval b)
+cat (List l1) (List l2) = return (List (l1 ++ l2))
+cat a b = errorE ("should either be both strings or both lists: " ++ show a ++ ", " ++ show b)
 
