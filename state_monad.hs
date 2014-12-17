4b98154adcf842237c557da822d35e706aea2838
C.J. Jennings <cfjennings3@gmail.com>
2014-12-17 12:21:04 -0500

    Add state monad module

---
+++ state_monad.hs
@@ -0,0 +1,16 @@
+module StateMonad where
+type State = Integer
+data ST a = St(State -> (a, State))
+
+instance Functor ST where
+	fmap f (St g) = St ((help f).g)
+
+help f (a, s) = (f a, s)
+
+instance Monad ST where
+	return a = St(\s -> (a,s))
+	St(f) >>= g = St(\s0 -> let (b,s1) = f s0 in
+		let St(h) = g b in
+			h s0)
+
+showST (St(t)) x = t x
